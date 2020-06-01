652ce49c7ebeafcb26f10195c38ea942304d2047
Dhruv Khandelwal <dhruv.khandelwal@zs.com>
2020-06-01 22:41:52 +0530

    Adding Challenge for June 1st

---
+++ JuneLeetcodingChallenge/src/InvertBinaryTree.java
@@ -0,0 +1,39 @@
+/*Invert a binary tree.
+
+Example:
+	Input:
+	
+	     4
+	   /   \
+	  2     7
+	 / \   / \
+	1   3 6   9
+	Output:
+	
+	     4
+	   /   \
+	  7     2
+	 / \   / \
+	9   6 3   1
+	Trivia:
+	This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary 
+tree on a whiteboard so f*** off.
+*/
+
+public class InvertBinaryTree {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null)
+			return null;
+
+		TreeNode left = invertTree(root.left);
+		TreeNode right = invertTree(root.right);
+
+		TreeNode temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+
+		return root;
+	}
+}
